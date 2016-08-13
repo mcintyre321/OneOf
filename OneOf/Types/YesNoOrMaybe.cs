29dcf4f7dac54f36466f4ecbf5c8e3fc04a74a72
mcintyre321 <mcintyre321@gmail.com>
2016-08-13 17:55:55 +0100

    Added some funky types!

---
+++ OneOf/Types/YesNoOrMaybe.cs
@@ -0,0 +1,16 @@
+﻿namespace OneOf.Types
+{
+    public class YesNoOrMaybe : OneOfBase<YesNoOrMaybe.Yes, YesNoOrMaybe.No, YesNoOrMaybe.Maybe>
+    {
+        public class Yes : YesNoOrMaybe { }
+        public class No : YesNoOrMaybe { }
+        public class Maybe : YesNoOrMaybe { }
+
+        public static implicit operator YesNoOrMaybe(bool? value)
+        {
+            return value == null ? new Maybe()
+                : value.Value ? (YesNoOrMaybe) new Yes()
+                    : new No();
+        }
+    }
+}
---
+++ OneOf/Types/YesOrNo.cs
@@ -0,0 +1,13 @@
+﻿namespace OneOf.Types
+{
+    public class YesOrNo : OneOfBase<YesOrNo.Yes, YesOrNo.No>
+    {
+        public class Yes : YesOrNo { }
+        public class No : YesOrNo { }
+
+        public static implicit operator YesOrNo(bool value)
+        {
+            return value ? (YesOrNo)new YesOrNo.Yes() : new YesOrNo.No();
+        }
+    }
+}
