2af170ec50b76a62755ec16723c9bea75189754c
Arno den Uijl <aduijl@sogyo.nl>
2017-09-03 22:18:29 +0200

    Added failing test for serialization of object that inherits from OneOfBase

---
+++ OneOf.Tests/OneOfBaseSerialization.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace OneOf.Tests
+{
+	public class OneOfBaseSerialization
+	{
+		[Test]
+		public void CanSerializeOneOfBaseValueTransparently()
+		{
+			//Given an object with a OneOf property 
+			var x = new SomeBaseThing.SomeConcreteThing{Value = "Some value"};
+
+			//When that object is serialized
+			var jsonSerializerSettings = new JsonSerializerSettings
+			{
+				Converters = {new OneOfJsonConverter()}
+			};
+			var json = JsonConvert.SerializeObject(x, jsonSerializerSettings);
+			var x2 = JsonConvert.DeserializeObject<SomeBaseThing.SomeConcreteThing>(json, jsonSerializerSettings);
+			Assert.AreEqual(x.Value, x2.Value);
+		}
+	}
+
+	public abstract class SomeBaseThing : OneOfBase<SomeBaseThing.SomeConcreteThing>
+	{
+		public class SomeConcreteThing : SomeBaseThing
+		{
+			public string Value { get; set; }
+		}
+	}
+}
