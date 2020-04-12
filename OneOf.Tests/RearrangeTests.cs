965f759abf2c3e62adf18ad66a8ec809633d33b6
Dai <dai@dai.me>
2020-04-11 20:42:36 -0700

    Whitespace.

--- OneOf.Tests/RearrangeTests.cs
+++ OneOf.Tests/RearrangeTests.cs
@@ -9,39 +9,39 @@ namespace OneOf.Tests
         [Test]
         public void CanRearrangeEquivalentOneOfValueTypesets()
         {
-			OneOf<byte,short,int,long> value = 123;
-			OneOf<long,int,short,byte> rearranged = OneOf<long,int,short,byte>.RearrangeFrom( value );
+            OneOf<byte,short,int,long> value = 123;
+            OneOf<long,int,short,byte> rearranged = OneOf<long,int,short,byte>.RearrangeFrom( value );
 
             bool isInt123 = false;
-			rearranged.Switch(
-				i64 => Assert.Fail(),
-				i32 => isInt123 = true,
-				i16 => Assert.Fail(),
-				u8  => Assert.Fail()
-			);
+            rearranged.Switch(
+                i64 => Assert.Fail(),
+                i32 => isInt123 = true,
+                i16 => Assert.Fail(),
+                u8  => Assert.Fail()
+            );
 
-			Assert.IsTrue( isInt123 );
+            Assert.IsTrue( isInt123 );
         }
 
-		/* Uncomment this to reveal a compilation/intellisense error that `value` cannot be used as an argument for `RearrangeFrom`.
+        /* Uncomment this to reveal a compilation/intellisense error that `value` cannot be used as an argument for `RearrangeFrom`.
 
-		[Test]
+        [Test]
         public void CannotRearrangeOneOfValuesWithDifferentValueTypeSets()
         {
-			OneOf<byte,short,int,long> value = 123;
-			OneOf<long,int,string,byte> rearranged = OneOf<long,int,string,byte>.RearrangeFrom( value );
+            OneOf<byte,short,int,long> value = 123;
+            OneOf<long,int,string,byte> rearranged = OneOf<long,int,string,byte>.RearrangeFrom( value );
 
             bool isInt123 = false;
-			rearranged.Switch(
-				i64 => Assert.Fail(),
-				i32 => isInt123 = true,
-				i16 => Assert.Fail(),
-				u8  => Assert.Fail()
-			);
+            rearranged.Switch(
+                i64 => Assert.Fail(),
+                i32 => isInt123 = true,
+                i16 => Assert.Fail(),
+                u8  => Assert.Fail()
+            );
 
-			Assert.IsTrue( isInt123 );
+            Assert.IsTrue( isInt123 );
         }
 
-		*/
+        */
     }
 }
