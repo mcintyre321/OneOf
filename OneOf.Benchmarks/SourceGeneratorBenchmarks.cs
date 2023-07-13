9f4df9751ec0499ff5857fdc71450f89be478eea
Timothy Makkison <timmakkison@gmail.com>
2023-07-13 15:55:28 +0100

    feat: add incremental pipeline caching

---
+++ OneOf.Benchmarks/SourceGeneratorBenchmarks.cs
@@ -0,0 +1,108 @@
+﻿
+using BenchmarkDotNet.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using OneOf.SourceGenerator;
+using System.Reflection;
+
+namespace OneOf.Benchmarks;
+
+[MemoryDiagnoser]
+[InProcess]
+[HtmlExporter]
+[JsonExporterAttribute.Full]
+public class SourceGeneratorBenchmarks
+{
+    private const string Small =
+        """
+        using OneOf;
+
+        namespace Foo
+        {
+            [GenerateOneOf]
+            public partial class Option : OneOfBase<int, string> { }
+        }
+        """;
+    
+    private const string Large =
+        """
+        using OneOf;
+
+        namespace Foo
+        {
+            [GenerateOneOf]
+            public partial class Option : OneOfBase<int, string> { }
+
+            [GenerateOneOf]
+            public partial class NestedGeneric : OneOfBase<List<List<string>>, List<int>, Dictionary<List<string>, string>> { }
+
+            [GenerateOneOf]
+            public partial class SimpleGeneric : OneOfBase<List<string>, List<int>, Dictionary<long, string>> { }
+
+            [GenerateOneOf]
+            internal partial class StringOrNumber : OneOfBase<string, int, uint> { }
+        }
+        """;
+    
+    private GeneratorDriver? _driver;
+    private Compilation? _compilation;
+
+    private static CSharpCompilation BuildCompilation(params SyntaxTree[] syntaxTrees)
+    {
+        var references = new List<MetadataReference>
+        {
+            MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(OneOfBase<>).GetTypeInfo().Assembly.Location),
+            MetadataReference.CreateFromFile(AppDomain.CurrentDomain.GetAssemblies().Single(a => a.GetName().Name == "netstandard").Location)
+        };
+
+        //https://github.com/dotnet/roslyn/issues/49498#issuecomment-734452762
+        foreach (var assembly in Assembly.GetEntryAssembly()!.GetReferencedAssemblies())
+        {
+            references.Add(MetadataReference.CreateFromFile(Assembly.Load(assembly).Location));
+        }
+
+        var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+        return CSharpCompilation.Create("Benchmark", syntaxTrees, references, compilationOptions);
+    }
+
+    private void BuildGenerator(string source)
+    {
+        var syntaxTree = CSharpSyntaxTree.ParseText(source, CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.Default));
+        _compilation = BuildCompilation(syntaxTree);
+
+        var generator = new OneOfGenerator();
+        _driver = CSharpGeneratorDriver.Create(generator);
+    }
+    
+    private void BuildIncremental(string source)
+    {
+        BuildGenerator(source);
+        _driver = _driver!.RunGeneratorsAndUpdateCompilation(_compilation!, out _, out _);
+    }
+
+    [GlobalSetup(Target = nameof(SmallCompile))]
+    public void SetupSmall() =>  BuildGenerator(Small);
+
+    [GlobalSetup(Target = nameof(LargeCompile))]
+    public void SetupLarge() =>  BuildGenerator(Large);
+    
+    [GlobalSetup(Target = nameof(SmallCompileIncremental))]
+    public void SetupSmallIncremental() =>  BuildIncremental(Small);
+
+    [GlobalSetup(Target = nameof(LargeCompileIncremental))]
+    public void SetupLargeIncremental() =>  BuildIncremental(Large);
+
+    [Benchmark]
+    public GeneratorDriver SmallCompile() => _driver!.RunGeneratorsAndUpdateCompilation(_compilation!, out _, out _);
+
+    [Benchmark]
+    public GeneratorDriver LargeCompile() => _driver!.RunGeneratorsAndUpdateCompilation(_compilation!, out _, out _);
+    
+    [Benchmark]
+    public GeneratorDriver SmallCompileIncremental() => _driver!.RunGeneratorsAndUpdateCompilation(_compilation!, out _, out _);
+
+    [Benchmark]
+    public GeneratorDriver LargeCompileIncremental() => _driver!.RunGeneratorsAndUpdateCompilation(_compilation!, out _, out _);
+}
---
+++ OneOf.SourceGenerator/ImmutableEquatableArray.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OneOf.SourceGenerator;
+
+public static class ImmutableEquatableArray
+{
+    public static ImmutableEquatableArray<T> Empty<T>()
+        where T : IEquatable<T> => ImmutableEquatableArray<T>.Empty;
+
+    public static ImmutableEquatableArray<T> ToImmutableEquatableArray<T>(this IEnumerable<T> values)
+        where T : IEquatable<T> => new(values);
+
+    public static ImmutableEquatableArray<T> Create<T>(params T[] values)
+        where T : IEquatable<T> => values is { Length: > 0 } ? new(values) : ImmutableEquatableArray<T>.Empty;
+}
+
+/// <summary>
+/// Provides an immutable list implementation which implements sequence equality.
+/// </summary>
+public sealed class ImmutableEquatableArray<T> : IEquatable<ImmutableEquatableArray<T>>, IReadOnlyList<T>
+    where T : IEquatable<T>
+{
+    public static ImmutableEquatableArray<T> Empty { get; } = new(Array.Empty<T>());
+
+    private readonly T[] _values;
+    public T this[int index] => _values[index];
+    public int Count => _values.Length;
+
+    public ImmutableEquatableArray(IEnumerable<T> values) => _values = values.ToArray();
+
+    public bool Equals(ImmutableEquatableArray<T>? other) =>
+        other != null && ((ReadOnlySpan<T>)_values).SequenceEqual(other._values);
+
+    public override bool Equals(object? obj) => obj is ImmutableEquatableArray<T> other && Equals(other);
+
+    public override int GetHashCode()
+    {
+        var hash = 0;
+        foreach (T value in _values)
+        {
+            hash = Combine(hash, value.GetHashCode());
+        }
+
+        static int Combine(int h1, int h2)
+        {
+            // RyuJIT optimizes this to use the ROL instruction
+            // Related GitHub pull request: https://github.com/dotnet/coreclr/pull/1830
+            uint rol5 = ((uint)h1 << 5) | ((uint)h1 >> 27);
+            return ((int)rol5 + h1) ^ h2;
+        }
+
+        return hash;
+    }
+
+    public Enumerator GetEnumerator() => new Enumerator(_values);
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>)_values).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => _values.GetEnumerator();
+
+    public struct Enumerator
+    {
+        private readonly T[] _values;
+        private int _index;
+
+        internal Enumerator(T[] values)
+        {
+            _values = values;
+            _index = -1;
+        }
+
+        public bool MoveNext()
+        {
+            var newIndex = _index + 1;
+
+            if ((uint)newIndex < (uint)_values.Length)
+            {
+                _index = newIndex;
+                return true;
+            }
+
+            return false;
+        }
+
+        public readonly T Current => _values[_index];
+    }
+}
